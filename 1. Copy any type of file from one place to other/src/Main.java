a91aff709a6928ab1f5b67189e5c2c49ec2c8208
Mohit Chachra <chachramohit@gmail.com>
2020-06-19 17:39:39 +0530

    Add files via upload

---
+++ 1. Copy any type of file from one place to other/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        //File file=new File("C:\\Users\\Mohit Chachra\\Desktop\\abc.txt");
+        //File file=new File("C:\\Users\\Mohit Chachra\\Desktop\\final222.pdf");
+        //File file=new File("C:\\Users\\Mohit Chachra\\Desktop\\hello.mp3");
+        File file=new File("C:\\Users\\Mohit Chachra\\Desktop\\startup weekend.docx");
+        FileInputStream fis=new FileInputStream(file);
+        //FileOutputStream fos=new FileOutputStream("C:\\Users\\Mohit Chachra\\Desktop\\bcde.txt");
+        //FileOutputStream fos=new FileOutputStream("C:\\Users\\Mohit Chachra\\Desktop\\abc.pdf");
+        //FileOutputStream fos=new FileOutputStream("C:\\Users\\Mohit Chachra\\Desktop\\bye.mp3");
+        FileOutputStream fos=new FileOutputStream("C:\\Users\\Mohit Chachra\\Desktop\\startup weekend.pdf");
+        int k;
+        while((k=fis.read())!=-1){
+            fos.write(k);
+        }
+
+    }
+}
